9ea9c15e93ebdfbe8ded20b41b1301a6d62c01bf
Ignacio Freilij <ignacio.freilij@gmail.com>
2024-02-28 10:50:39 -0300

    Parcial-IgnacioFreilij

---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Card = () => {
+  return <div>Soy una card</div>;
+};
+
+export default Card;
---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div>
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+    </div>
+  );
+};
+
+export default Form;
