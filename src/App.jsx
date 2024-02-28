49fe649958d0893289323e2a217f9d556b59a1c5
Ignacio Freilij <ignacio.freilij@gmail.com>
2024-02-28 20:11:06 -0300

    examen parcial front 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,61 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Card from "./components/Card";
-import Form from "./components/Form";
 
 function App() {
+  const [animal, setAnimal] = useState({
+    mascota: "",
+    color: "",
+  });
+  const [show, setShow] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (
+      animal.mascota[0] !== " " &&
+      animal.mascota.length >= 3 &&
+      animal.color.length >= 6
+    ) {
+      setShow(true);
+      setError(false);
+    } else {
+      setShow(false);
+      setError(true);
+    }
+  };
+
+  console.log(animal);
+  console.log("primera letra " + animal.mascota[0]);
+  console.log("show " + show);
+  console.log("error " + error);
   return (
     <>
-      <Form />
-      <Card />
+      <form onSubmit={handleSubmit}>
+        <label>Mascota: </label>
+        <input
+          type="text"
+          onChange={({ target }) =>
+            setAnimal({ ...animal, mascota: target.value })
+          }
+        /> 
+        <hr></hr>
+        <label>Color: </label>
+        <input
+          type="text"
+          onChange={({ target }) =>
+            setAnimal({ ...animal, color: target.value })
+          }
+        />
+             <hr></hr>
+        <button>Enviar</button>
+      </form>
+      <>
+        {show && <Card mascota={animal.mascota} color={animal.color} />}
+
+        {error && <h4 style={{color:"red"}}>Por favor chequea que la información sea correcta</h4>}
+      </>
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
-const Card = () => {
-  return <div>Soy una card</div>;
+const Card = ({mascota, color}) => {
+  return <div>
+    <h2>La mascota es: {mascota}</h2>
+    <h2>El color es: {color}</h2>
+  </div>;
 };
 
 export default Card;
